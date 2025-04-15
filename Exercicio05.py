69ae146e9537b1ebf39febfc5e128c9947fca97a
Gustavo Souza <gggutavo1301@gmail.com>
2025-04-15 09:50:50 -0300

    Sistema de divisão simples, e valor2 não pode ser 0, usando o while

---
+++ Exercicio05.py
@@ -0,0 +1,8 @@
+valor1 = int(input("num:"))
+valor2 = int(input("num2:"))
+while valor2 == 0:
+    print("Digite um número diferente de 0")
+    valor2 = int(input("num2:"))
+
+soma = valor1 / valor2
+print(soma)
